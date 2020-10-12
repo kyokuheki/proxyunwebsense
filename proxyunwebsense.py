df311776974187fbb1900826442531a3837715c9
kyokuheki <kyokuheki@gmail.com>
2020-10-12 11:37:24 +0900

    update proxyunwebsense.py

--- proxyunwebsense.py
+++ proxyunwebsense.py
@@ -8,8 +8,10 @@ import argparse
 import requests
 import lxml.html
 
-logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(name)s: %(funcName)s(%(lineno)d): %(message)s")
-#logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(levelname)s %(name)s:%(funcName)s(%(filename)s:%(lineno)d): %(message)s")
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(name)s: %(funcName)s(%(lineno)d): %(message)s")
+# logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(levelname)s %(name)s:%(funcName)s(%(filename)s:%(lineno)d): %(message)s")
 logger = logging.getLogger("proxyunwebsence")
 proxies = {}
 
@@ -22,24 +24,25 @@ DEFAULT_HEADERS = {
     'Cache-Control': 'max-age=0',
 }
 
-#from http.client import HTTPSConnection, RemoteDisconnected
-#def conn(proxy_host, proxy_port, host, port=443, debuglevel=0):
-#    c = HTTPSConnection(proxy_host, proxy_port,
-#                        context=_create_unverified_context())
-#    c.set_debuglevel(debuglevel)
-#    c.set_tunnel(host, port)
-#    return c
-#def req(proxy_host, proxy_port, url):
-#    _c = conn(proxy_host, proxy_port, url.netloc)
-#    headers = dict(DEFAULT_HEADERS)
-#    headers['Host'] = url.netloc
-#    _c.request('HEAD', '{}?{}'.format(url.path, url.query), headers=headers)
-#    try:
-#        _r = _c.getresponse()
-#    except Exception as _e:
-#        logger.exception(_e)
-#        raise
-#    return _r
+# from http.client import HTTPSConnection, RemoteDisconnected
+# def conn(proxy_host, proxy_port, host, port=443, debuglevel=0):
+#     c = HTTPSConnection(proxy_host, proxy_port,
+#                         context=_create_unverified_context())
+#     c.set_debuglevel(debuglevel)
+#     c.set_tunnel(host, port)
+#     return c
+# def req(proxy_host, proxy_port, url):
+#     _c = conn(proxy_host, proxy_port, url.netloc)
+#     headers = dict(DEFAULT_HEADERS)
+#     headers['Host'] = url.netloc
+#     _c.request('HEAD', '{}?{}'.format(url.path, url.query), headers=headers)
+#     try:
+#         _r = _c.getresponse()
+#     except Exception as _e:
+#         logger.exception(_e)
+#         raise
+#     return _r
+
 
 def unwebsense(url: urllib.parse.ParseResult):
     request_url = url.geturl()
@@ -65,19 +68,24 @@ def unwebsense(url: urllib.parse.ParseResult):
         data[e.attrib['name']] = e.attrib['value']
     elements = h.xpath('//*[@id="contents"]/form')
     path = elements[0].attrib['action']
-    method = elements[0].attrib['method']
+    # method = elements[0].attrib['method']
 
     logger.info("stage 3")
     base_url = h.base_url if h.base_url else r.url
     url = urllib.parse.urljoin(base_url, path)
-    r = requests.post(url, data=data, allow_redirects=False, proxies=proxies, headers=DEFAULT_HEADERS)
+    r = requests.post(
+        url,
+        data=data,
+        allow_redirects=False,
+        proxies=proxies,
+        headers=DEFAULT_HEADERS)
     r.raise_for_status()
     logger.info("POST: {} {} ".format(url, data))
     logger.info("POST: status code: {}".format(r.status_code))
     if r.status_code == requests.codes.found:
         logger.info("unwebsence: {}".format(request_url))
-
     return r
+
 
 def is_blocked(url: urllib.parse.ParseResult):
     request_url = url.geturl()
@@ -102,18 +110,42 @@ def is_blocked(url: urllib.parse.ParseResult):
             return False
     h = lxml.html.fromstring((r.text))
     logger.info("GET: status code: {}".format(r.status_code))
-    return r.status_code == requests.codes.ok and h.find(".//title").text == 'Blocked by Websense'
+    return r.status_code == requests.codes.ok and h.find(
+        ".//title").text == 'Blocked by Websense'
+
 
 def main():
     parser = argparse.ArgumentParser(
         description='proxyunwebsence.py: Unblock proxy websence.')
-    parser.add_argument('--http_proxy', action="store", type=urllib.parse.urlparse,
-                        default=os.getenv("HTTP_PROXY", "http://127.0.0.1:3128"), help="HTTP PROXY")
-    parser.add_argument('--https_proxy', action="store", type=urllib.parse.urlparse,
-                        default=os.getenv("HTTPS_PROXY", "http://127.0.0.1:3128"), help="HTTPS PROXY")
-    parser.add_argument('-c', '--count', action="store",
-                        type=int, default=5, help="set the number of unwebsence.")
-    parser.add_argument('url', nargs='*', type=urllib.parse.urlparse, default=[urllib.parse.urlparse("https://example.com")])
+    parser.add_argument(
+        '--http_proxy',
+        action="store",
+        type=urllib.parse.urlparse,
+        default=os.getenv(
+            "HTTP_PROXY",
+            "http://127.0.0.1:3128"),
+        help="HTTP PROXY")
+    parser.add_argument(
+        '--https_proxy',
+        action="store",
+        type=urllib.parse.urlparse,
+        default=os.getenv(
+            "HTTPS_PROXY",
+            "http://127.0.0.1:3128"),
+        help="HTTPS PROXY")
+    parser.add_argument(
+        '-c',
+        '--count',
+        action="store",
+        type=int,
+        default=5,
+        help="set the number of unwebsence.")
+    parser.add_argument(
+        'url',
+        nargs='*',
+        type=urllib.parse.urlparse,
+        default=[
+            urllib.parse.urlparse("https://example.com")])
 
     args = parser.parse_args()
     logger.info("args: {}".format(args))
@@ -129,12 +161,13 @@ def main():
         if is_blocked(url) or args.count > 1:
             for i in range(args.count):
                 logger.info("count: {}".format(i))
-                r = unwebsense(url)
+                unwebsense(url)
                 print("unwebsense: {}".format(url.geturl()))
             exit(is_blocked(url))
         else:
             print("websense not found")
             exit()
+
 
 if __name__ == "__main__":
     main()
